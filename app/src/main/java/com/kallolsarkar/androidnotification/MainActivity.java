2df2619e0d54b1d34ef9ce1a31fafa6a75154d62
KallolSarkar <kallol.sarkr@gmail.com>
2018-11-08 22:50:33 +0530

    Set button 1 onclick

--- app/src/main/java/com/kallolsarkar/androidnotification/MainActivity.java
+++ app/src/main/java/com/kallolsarkar/androidnotification/MainActivity.java
@@ -1,13 +1,46 @@
 package com.kallolsarkar.androidnotification;
 
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.support.v4.app.NotificationCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button b1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        b1 =findViewById(R.id.show);
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sendNotification();
+            }
+        });
+    }
+
+    public void sendNotification() {
+
+        //Get an instance of NotificationManager//
+
+        NotificationCompat.Builder mBuilder =
+                new NotificationCompat.Builder(this)
+                        .setSmallIcon(R.drawable.ic_launcher_background)
+                        .setContentTitle("My notification")
+                        .setContentText("Hello World!");
+
+        NotificationManager mNotificationManager =
+
+                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+
     }
 }
